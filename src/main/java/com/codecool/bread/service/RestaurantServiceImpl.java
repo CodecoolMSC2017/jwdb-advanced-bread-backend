da248cc2f548b59d6c28a54c1fae082744af822d
norcs <norbertcs@me.com>
2018-07-10 20:56:35 +0200

    Implement RestaurantService interface's two methods

---
+++ src/main/java/com/codecool/bread/service/RestaurantServiceImpl.java
@@ -0,0 +1,31 @@
+package com.codecool.bread.service;
+
+import com.codecool.bread.exception.OwnerNotFoundException;
+import com.codecool.bread.exception.RestaurantNotFoundException;
+import com.codecool.bread.model.Restaurant;
+import com.codecool.bread.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("restaurantService")
+public class RestaurantServiceImpl implements RestaurantService {
+
+    @Autowired
+    RestaurantRepository restaurantRepository;
+
+    @Override
+    public List<Restaurant> getAllRestaurants() {
+        return restaurantRepository.findAll();
+    }
+
+    @Override
+    public Restaurant getRestaurantById(int id) {
+        if(restaurantRepository.findById(id).isPresent()) {
+            return restaurantRepository.findById(id).get();
+        }else {
+            throw new RestaurantNotFoundException("Not found");
+        }
+    }
+}
