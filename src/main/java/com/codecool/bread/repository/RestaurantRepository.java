ee61cc0c3280612dbb9637220500289e08047391
norcs <norbertcs@me.com>
2018-07-10 20:54:44 +0200

    Add RestaurantRepository interface

--- src/main/java/com/codecool/bread/repository/RestaurantRepository.java
+++ src/main/java/com/codecool/bread/repository/RestaurantRepository.java
@@ -4,6 +4,6 @@ import com.codecool.bread.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository("restaurantRepository")
 public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
 }
