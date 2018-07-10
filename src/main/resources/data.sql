4b8ddf0dbbd937b88f0a119465f0f408d27a4d9c
norcs <norbertcs@me.com>
2018-07-10 20:56:51 +0200

    Fix typo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@ INSERT INTO owner(first_name, last_name, email, password ) VALUES
 	('Robert', 'King', 'robert.king@gmail.com', 'password'), --1
 	('Nancy', 'Davolio', 'nancy.davolio@gmail.com', 'password'); --2
 
-INSERT INTO address(street, city, postcode, state, country) VALUES
+INSERT INTO address(street, city, postal_code, state, country) VALUES
 	('4110 Old Redmond Rd.', 'Redmond', 98502, 'Washington', 'USA'); --1
 
 INSERT INTO restaurant(owner_id, name, email, address_id, phone) VALUES
