47cd78a8a0916ddffb58cca5f024a80060fc0e38
Kenezke1 <herczeg.kenya2@gmail.com>
2018-07-11 10:22:22 +0200

    ADD annotations to custom exception

--- src/main/java/com/codecool/bread/exception/RestaurantNotFoundException.java
+++ src/main/java/com/codecool/bread/exception/RestaurantNotFoundException.java
@@ -1,7 +1,12 @@
 package com.codecool.bread.exception;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class RestaurantNotFoundException extends RuntimeException {
-    public RestaurantNotFoundException(String message) {
-        super(message);
+    public RestaurantNotFoundException() {
+        super("Restaurant not found");
     }
 }
--- src/main/java/com/codecool/bread/service/exception/RestaurantNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.codecool.bread.service.exception;
-
-public class RestaurantNotFoundException extends Throwable {
-    public RestaurantNotFoundException(String message) {
-        super(message);
-    }
-}
