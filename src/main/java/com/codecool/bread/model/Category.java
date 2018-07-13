458ff884b1419d27a27e0283de77b08b55aed9cb
Gergely Murvai <murvai.gergely@gmail.com>
2018-07-13 20:45:30 +0200

    Added new categories

--- src/main/java/com/codecool/bread/model/Category.java
+++ src/main/java/com/codecool/bread/model/Category.java
@@ -1,12 +1,16 @@
 package com.codecool.bread.model;
 
 public enum Category {
+    PIZZA,
     APPETIZER,
     SOUP,
     BEEF_DISH,
     PORK_DISH,
     CHICKEN_DISH,
+    FISH_MAIN,
+    VEGETARIAN,
     BEVERAGE,
     ALCOHOLIC,
-    COFFEE
+    COFFEE,
+    TEA
 }
