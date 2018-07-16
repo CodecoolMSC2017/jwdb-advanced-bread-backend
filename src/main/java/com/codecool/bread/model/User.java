3fe0dc05717145cce857c8f5d9cb4ddbdb9fd331
norcs <norbertcs@me.com>
2018-07-16 16:27:13 +0200

    Remove password and enabled fields

--- src/main/java/com/codecool/bread/model/User.java
+++ src/main/java/com/codecool/bread/model/User.java
@@ -14,33 +14,11 @@ public class User {
     @Column(name = "username", nullable = false, unique = true)
     private String username;
 
-    @JsonIgnore
-    private String password;
-
-    @JsonIgnore
-    private boolean enabled;
-
     public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
     }
 }
