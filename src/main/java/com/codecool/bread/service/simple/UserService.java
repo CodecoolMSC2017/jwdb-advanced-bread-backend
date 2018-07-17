f6683ccc255598a5e279cdc9b50a0643c32cd31f
norcs <norbertcs@me.com>
2018-07-17 16:16:59 +0200

    Add UserService interface

---
+++ src/main/java/com/codecool/bread/service/simple/UserService.java
@@ -0,0 +1,22 @@
+package com.codecool.bread.service.simple;
+
+import com.codecool.bread.model.User;
+
+import java.util.Optional;
+
+public interface UserService {
+
+    Optional<User> get(Integer id);
+
+    void delete(Integer id);
+
+    Iterable<User> getAll();
+
+    Optional<User> get(String username);
+
+    User add(String username, String password, String confirmationPassword);
+
+    void changePassword(String oldPassword, String newPassword, String confirmationPassword);
+
+
+}
