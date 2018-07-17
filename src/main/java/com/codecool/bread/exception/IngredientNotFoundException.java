c7ce2ed0245a64ed7b82cf40b2759a1bd5df98db
norcs <norbertcs@me.com>
2018-07-17 10:02:20 +0200

    Update error codes

--- src/main/java/com/codecool/bread/exception/IngredientNotFoundException.java
+++ src/main/java/com/codecool/bread/exception/IngredientNotFoundException.java
@@ -3,7 +3,7 @@ package com.codecool.bread.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class IngredientNotFoundException extends RuntimeException {
     public IngredientNotFoundException() {
         super("Ingredient not found");
--- src/main/java/com/codecool/bread/exception/NoItemsFoundException.java
+++ src/main/java/com/codecool/bread/exception/NoItemsFoundException.java
@@ -3,7 +3,7 @@ package com.codecool.bread.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NoItemsFoundException extends RuntimeException {
     public NoItemsFoundException() {
         super("There are no items added to this restaurant");
