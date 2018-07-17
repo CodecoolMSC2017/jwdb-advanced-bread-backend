0724124fac2f905424f4218ceff0a463cb11bcf6
norcs <norbertcs@me.com>
2018-07-17 16:13:49 +0200

    Add authorities and id fields, getters and setters

--- src/main/java/com/codecool/bread/model/User.java
+++ src/main/java/com/codecool/bread/model/User.java
@@ -1,18 +1,38 @@
 package com.codecool.bread.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements Serializable {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
     @Column(name = "username", nullable = false, unique = true)
     private String username;
+
+    private boolean enabled;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "authorities",
+            joinColumns = @JoinColumn(name = "username", referencedColumnName = "username")
+    )
+    @Column(name = "authority")
+    private List<String> authorities;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getUsername() {
         return username;
@@ -20,5 +40,21 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<String> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(List<String> authorities) {
+        this.authorities = authorities;
     }
 }
