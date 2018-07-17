b55c173f7181fd283f2976e6f682608b8d4a1e6a
norcs <norbertcs@me.com>
2018-07-17 16:18:13 +0200

    Add AuthenticationManager and configure webSecurity

--- src/main/java/com/codecool/bread/SecurityConfig.java
+++ src/main/java/com/codecool/bread/SecurityConfig.java
@@ -3,18 +3,21 @@ package com.codecool.bread;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import javax.sql.DataSource;
+
+import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
@@ -37,50 +40,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().permitAll()
                 .and()
                 .logout().permitAll()
-                .and()
-                .exceptionHandling().accessDeniedPage("/403")
                 .and().httpBasic();
-        http.csrf().disable();
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 
     }
-        /*
-        http.authorizeRequests()
-                .antMatchers("/error", "/", "/index").permitAll()
-                .antMatchers("/owner/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
 
-                .antMatchers("/resources/**", "/signup", "/about").permitAll()                  2
-                .antMatchers("/admin/**").hasRole("ADMIN")                                      3
-                .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")            4
-                .anyRequest().authenticated()
-
-        http.csrf().disable();
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception {
+        webSecurity.ignoring().antMatchers(POST, "/owner/user");
     }
-*/
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    UserDetailsManager userDetailsManager(
+            @Autowired DataSource dataSource,
+            @Autowired JdbcTemplate jdbcTemplate,
+            @Autowired AuthenticationManager authenticationManager) {
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager();
+        userDetailsManager.setJdbcTemplate(jdbcTemplate);
+        userDetailsManager.setDataSource(dataSource);
+        // Using AuthenticationManager re-authenticates the session on password change.
+        userDetailsManager.setAuthenticationManager(authenticationManager);
+        return userDetailsManager;
+    }
     @Bean
     public BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
-    /*
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new PasswordEncoder() {
-            @Override
-            public String encode(CharSequence charSequence) {
-                return (String)charSequence;
-            }
-
-            @Override
-            public boolean matches(CharSequence charSequence, String s) {
-                return s.equals(charSequence);
-            }
-        };
-    }
-    */
 }
