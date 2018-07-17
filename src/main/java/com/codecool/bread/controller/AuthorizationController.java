79ce99ab482d12a9485f8ffa52feaf8e48b04e77
norcs <norbertcs@me.com>
2018-07-17 16:07:04 +0200

    Add AuthorizationController

---
+++ src/main/java/com/codecool/bread/controller/AuthorizationController.java
@@ -0,0 +1,31 @@
+package com.codecool.bread.controller;
+
+import com.codecool.bread.model.User;
+import com.codecool.bread.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthorizationController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("")
+    public Optional<User> get(Principal principal) {
+        return userService.get(principal.getName());
+    }
+
+    @DeleteMapping("")
+    public void delete(HttpSession session) {
+        session.invalidate();
+    }
+}
