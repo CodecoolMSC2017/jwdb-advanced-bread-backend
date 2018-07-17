4bf53fc3089b2ec8fba6de828923c169aac99197
norcs <norbertcs@me.com>
2018-07-17 16:09:06 +0200

    Reformat code

--- src/main/java/com/codecool/bread/model/Employee.java
+++ src/main/java/com/codecool/bread/model/Employee.java
@@ -11,21 +11,26 @@ import javax.persistence.Table;
 public class Employee extends POSObject {
 
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name = "username")
+    @JoinColumn(name = "user_id")
     private User user;
+
     private String email;
+
     @Column(name = "first_name")
     private String firstName;
+
     @Column(name = "last_name")
     private String lastName;
+
     @Column(name = "title")
     @Enumerated(EnumType.STRING)
+
     private Role role;
+
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "restaurant_id", nullable = false)
     @JsonIgnore
     private Restaurant restaurant;
-
 
     public String getEmail() {
         return email;
