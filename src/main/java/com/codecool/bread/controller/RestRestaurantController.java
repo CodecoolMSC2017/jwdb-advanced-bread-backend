412658d7250447304b01ca063491a267f6a5e477
norcs <norbertcs@me.com>
2018-07-19 22:15:11 +0200

    Add TODOs

--- src/main/java/com/codecool/bread/controller/RestRestaurantController.java
+++ src/main/java/com/codecool/bread/controller/RestRestaurantController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/restaurant/{restaurantId}")
+@RequestMapping("/restaurant/{restaurantId}") // TODO ???? service methods don't work without ownerID, have to be fixed
 public class RestRestaurantController {
 
     @Autowired
