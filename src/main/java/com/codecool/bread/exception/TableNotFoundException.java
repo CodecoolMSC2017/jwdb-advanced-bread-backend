48c65a5bd78a99cdd498862a09ad8c3c53d1052e
Gergely Murvai <murvai.gergely@gmail.com>
2018-07-25 11:57:06 +0200

    Added @ResponseStatus annotation 404

--- src/main/java/com/codecool/bread/exception/TableNotFoundException.java
+++ src/main/java/com/codecool/bread/exception/TableNotFoundException.java
@@ -1,5 +1,9 @@
 package com.codecool.bread.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class TableNotFoundException extends RuntimeException {
     public TableNotFoundException() {
         super("No table found with this id");
