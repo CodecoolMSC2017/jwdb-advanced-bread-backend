f0d3b5878901c828eae0b5f10bcc7366ca73968d
Gergely Murvai <murvai.gergely@gmail.com>
2018-07-25 16:02:40 +0200

    replaced permitAll() to log in properly with any role

--- src/main/java/com/codecool/bread/controller/AbstractController.java
+++ src/main/java/com/codecool/bread/controller/AbstractController.java
@@ -1,5 +1,9 @@
 package com.codecool.bread.controller;
 
+import com.codecool.bread.exception.EmployeeNotFoundException;
+import com.codecool.bread.exception.OwnerNotFoundException;
+import com.codecool.bread.exception.UserNotFoundException;
+import com.codecool.bread.service.EmployeeService;
 import com.codecool.bread.service.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,8 +14,24 @@ public abstract class AbstractController {
     @Autowired
     private OwnerService ownerService;
 
+    @Autowired
+    private EmployeeService employeeService;
+
     int getLoggedInOwnerId(Principal principal) {
-        return ownerService.getOwnerById(ownerService.getOwnerByUsername(principal.getName()).getId()).getId();
+        int userId = 0;
+        try {
+            userId = ownerService.getOwnerById(ownerService.getOwnerByUsername(principal.getName()).getId()).getId();
+            return userId;
+        } catch (OwnerNotFoundException o){
+            try{
+                userId = employeeService.getEmployeeById(employeeService.getEmployeeByUsername(principal.getName()).getId()).getId();
+                return userId;
+            } catch (EmployeeNotFoundException e) {
+                throw new UserNotFoundException();
+            }
+        }
+
 
     }
+
 }
