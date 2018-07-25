dc2cfb312e0f8668f6e96d7bbfddcd1ddf4bac7a
Gergely Murvai <murvai.gergely@gmail.com>
2018-07-25 10:12:44 +0200

    getEnableItemsByRestaurantId() added to the ItemService interface and implemented in ItemServiceImpl

--- src/main/java/com/codecool/bread/service/ItemService.java
+++ src/main/java/com/codecool/bread/service/ItemService.java
@@ -12,6 +12,7 @@ import java.util.List;
 public interface ItemService {
 
     List<Item> getItemsByRestaurantId(Integer restaurantId) throws NoItemsFoundException;
+    List<Item> getEnableItemsByRestaurantId(Integer restaurantId) throws NoItemsFoundException;
     Item getItemById(Integer id, Integer restaurantId) throws ItemAccessDeniedException, NoItemsFoundException;
     Item addNewItem(Item item, int restaurantId);
     void deleteItem(int restaurantId, int itemId) throws RestaurantAccessDeniedException, ItemNotFoundException;
