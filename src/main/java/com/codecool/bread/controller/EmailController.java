7bb62c0de73167c0f5df61123cf5569c3e63f608
Gergely Murvai <murvai.gergely@gmail.com>
2018-07-30 13:42:35 +0200

    Basic emailService created

---
+++ src/main/java/com/codecool/bread/controller/EmailController.java
@@ -0,0 +1,33 @@
+package com.codecool.bread.controller;
+
+import com.codecool.bread.model.Email;
+import com.codecool.bread.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.mail.SendFailedException;
+
+@Controller
+@RequestMapping("/send")
+public class EmailController {
+
+    @Autowired
+    private EmailService emailServive;
+
+    @PostMapping("/invite")
+    public ResponseEntity<?> sendInvitation(@RequestBody Email email) {
+        try {
+            emailServive.sendSimpleMessage(email);
+            return ResponseEntity.ok("Success");
+        } catch (SendFailedException ex) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+}
