d2f4c859ac998348cc2130e2b6a24f726a4b971e
Gergely Murvai <murvai.gergely@gmail.com>
2018-08-02 14:50:20 +0200

    Minor changes in exception handling

--- src/main/java/com/codecool/bread/exception/PasswordNotMatchException.java
+++ src/main/java/com/codecool/bread/exception/PasswordNotMatchException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
-public class UsernameTakenException extends RuntimeException{
-    public UsernameTakenException(String message) {
-        super(message + "already taken. Choose another username");
+public class PasswordNotMatchException extends RuntimeException {
+    public PasswordNotMatchException(){
+        super("Password and confirmation needs to be the same!");
     }
 }
