7eac94c417c55d62c56b7163e35606c99423f6af
zsarna2 <zsarna2@gmail.com>
2018-08-29 11:24:22 +0200

    income getter now get ownerId from principal

--- src/main/java/com/codecool/bread/controller/StatisticsController.java
+++ src/main/java/com/codecool/bread/controller/StatisticsController.java
@@ -6,37 +6,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping("/stats")
-public class StatisticsController {
+public class StatisticsController extends AbstractController {
 
     @Autowired
     private StatisticsService statisticsService;
 
-    @GetMapping("/owner/{ownerId}/restaurants/avg/income")
-    public List<StatsDto> getAllRestaurantIncomeAvg(@PathVariable("ownerId") int ownerId, @RequestParam("start")
+    @GetMapping("/owner/restaurants/avg/income")
+    public List<StatsDto> getAllRestaurantIncomeAvg(Principal principal, @RequestParam("start")
     @DateTimeFormat(pattern = "yyyy-MM-dd") Date start, @RequestParam("end") @DateTimeFormat(pattern = "yyyy-MM-dd") Date end) {
-        return statisticsService.getAllRestaurantIncomeAvgFromDb(ownerId, start, end);
+        return statisticsService.getAllRestaurantIncomeAvgFromDb(getLoggedInEmployeeId(principal), start, end);
     }
 
-    @GetMapping("/owner/{ownerId}/restaurants/sum/income")
-    public List<StatsDto> getAllRestaurantIncomeSum(@PathVariable("ownerId") int ownerId, @RequestParam("start")
+    @GetMapping("/owner/restaurants/sum/income")
+    public List<StatsDto> getAllRestaurantIncomeSum(Principal principal, @RequestParam("start")
     @DateTimeFormat(pattern = "yyyy-MM-dd") Date start, @RequestParam("end") @DateTimeFormat(pattern = "yyyy-MM-dd") Date end) {
-        return statisticsService.getAllRestaurantIncomeSumFromDb(ownerId, start, end);
+        return statisticsService.getAllRestaurantIncomeSumFromDb(getLoggedInEmployeeId(principal), start, end);
     }
 
     @GetMapping("/order/orderitemquantity/{restaurantId}")
     public List<StatsDto> getOrderQuantity(@PathVariable("restaurantId") int restaurantId,
-    @DateTimeFormat(pattern = "yyyy-MM-dd") Date start, @RequestParam("end") @DateTimeFormat(pattern = "yyyy-MM-dd") Date end) {
+                                           @DateTimeFormat(pattern = "yyyy-MM-dd") Date start, @RequestParam("end") @DateTimeFormat(pattern = "yyyy-MM-dd") Date end) {
         return statisticsService.getOrderQuantityByItemIdFromDb(restaurantId, start, end);
     }
 
     @GetMapping("/numofguests/{restaurantId}")
     public List<StatsDto> getNumOfGuests(@PathVariable("restaurantId") int restaurantId,
-                                           @DateTimeFormat(pattern = "yyyy-MM-dd") Date start, @RequestParam("end") @DateTimeFormat(pattern = "yyyy-MM-dd") Date end) {
+                                         @DateTimeFormat(pattern = "yyyy-MM-dd") Date start, @RequestParam("end") @DateTimeFormat(pattern = "yyyy-MM-dd") Date end) {
         return statisticsService.getNumOfGuestsFromDb(restaurantId, start, end);
     }
 }
